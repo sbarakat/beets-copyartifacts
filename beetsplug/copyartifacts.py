b72898ee4aa6269fbf5c6ac91110eca60c45c32e
Sami Barakat <sami@sbarakat.co.uk>
2014-02-16 21:41:41 +0000

    Use some beet.util magic and bumped version to 0.2

--- beetsplug/copyartifacts.py
+++ beetsplug/copyartifacts.py
@@ -1,6 +1,6 @@
 import os
-import shutil
 
+import beets.util
 from beets.plugins import BeetsPlugin
 
 class CopyArtifactsPlugin(BeetsPlugin):
@@ -16,22 +16,6 @@ class CopyArtifactsPlugin(BeetsPlugin):
         self.print_ignored = self.config['print_ignored'].get()
 
         self.register_listener('import_task_files', self.add_artifacts)
-
-    def _find_valid_filename(self, file_path):
-        if os.path.exists(file_path):
-            path = os.path.dirname(file_path)
-            filename = os.path.basename(file_path)
-
-            split = os.path.splitext(filename)
-            for i in range(1, 30):
-                try_file = '.{0}'.format(i).join(split)
-                if not os.path.exists(os.path.join(path, try_file)):
-                    file_path = os.path.join(path, try_file)
-                    return file_path
-            else:
-                return None
-
-        return file_path
 
     def add_artifacts(self, task, session):
         # there has to be a better way of doing this
@@ -57,20 +41,13 @@ class CopyArtifactsPlugin(BeetsPlugin):
             print 'Copying artifacts:'
             for source_file in source_files:
                 filename = os.path.basename(source_file)
+                dest_file = beets.util.unique_path(os.path.join(dest_path, filename))
 
-                dest_file = os.path.join(dest_path, filename)
-                dest_file = self._find_valid_filename(dest_file)
-
-                if dest_file:
-                    print '   ', os.path.basename(dest_file)
-                    shutil.copy2(source_file, dest_file)
-                else:
-                    print ('Error: too many artifacts exist with the same ' +
-                           'filename, not copying', filename)
+                print '   ', os.path.basename(dest_file)
+                beets.util.copy(source_file, dest_file)
 
         if self.print_ignored and ignored_files:
             print 'Ignored files:'
             for f in ignored_files:
-                filename = os.path.basename(f)
-                print '   ', filename
+                print '   ', os.path.basename(f)
 
