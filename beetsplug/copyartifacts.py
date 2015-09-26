1dd556fe4917238c7e8f30787887d2403024d95f
Sami Barakat <sami@sbarakat.co.uk>
2015-09-26 15:38:05 +0100

    queue dirs for processing on cli_exit event to avoid clashes with other plugins or beets

--- beetsplug/copyartifacts.py
+++ beetsplug/copyartifacts.py
@@ -22,13 +22,18 @@ class CopyArtifactsPlugin(BeetsPlugin):
             'print_ignored': False
         })
 
+        self._process_queue = []
+        self._dirs_seen = []
+
         self.extensions = self.config['extensions'].as_str_seq()
         self.print_ignored = self.config['print_ignored'].get()
 
         self.path_formats = [c for c in beets.ui.get_path_formats() if c[0][:4] == u'ext:']
 
         self.register_listener('import_task_files', self.import_event)
+        # XXX: now that we have a moved event, is there any need in the import event?
         self.register_listener('item_moved', self.move_event)
+        self.register_listener('cli_exit', self.process_events)
 
     def _destination(self, filename, mapping):
         '''Returns a destination path a file should be moved to. The filename
@@ -85,11 +90,11 @@ class CopyArtifactsPlugin(BeetsPlugin):
         return mapping
 
     def import_event(self, task, session):
+        # TODO: import_event is called after all move_events
+        print 'import_event'
         imported_item = task.imported_items()[0]
         album_path = os.path.dirname(imported_item.path)
 
-        source_files = []
-        ignored_files = []
         reimport = False
 
         source_path = ''
@@ -98,6 +103,11 @@ class CopyArtifactsPlugin(BeetsPlugin):
         except TypeError:
             source_path = os.path.dirname(task.old_paths[0])
 
+        # Check if this path has already been processed
+        if source_path in self._dirs_seen:
+            return
+
+        non_handled_files = []
         for root, dirs, files in beets.util.sorted_walk(
                     source_path, ignore=config['ignore'].as_str_seq()):
             for filename in files:
@@ -112,25 +122,30 @@ class CopyArtifactsPlugin(BeetsPlugin):
                 if len(file_ext) > 1 and file_ext[1:] in TYPES:
                     continue
 
-                if '.*' in self.extensions or file_ext in self.extensions:
-                    source_files.append(source_file)
-                else:
-                    ignored_files.append(source_file)
+                non_handled_files.append(source_file)
 
-        mapping = self._generate_mapping(imported_item, album_path)
         if task.replaced_items[imported_item]:
             # these will be reimports when dir has changed
             reimport = True
 
-        self.process_artifacts(source_path, source_files, ignored_files, reimport, mapping)
+        self._process_queue.extend([{
+            'files': non_handled_files,
+            'mapping': self._generate_mapping(imported_item, album_path)
+            # TODO: add reimport var, may be useless if we get rid of
+            # import_event()
+        }])
+        self._dirs_seen.extend([source_path])
 
     def move_event(self, item, source, destination):
+        print 'move_event'
         source_path = os.path.dirname(source)
-        album_path = os.path.dirname(destination)
+        dest_path = os.path.dirname(destination)
 
-        source_files = []
-        ignored_files = []
+        # Check if this path has already been processed
+        if source_path in self._dirs_seen:
+            return
 
+        non_handled_files = []
         for root, dirs, files in beets.util.sorted_walk(
                     source_path, ignore=config['ignore'].as_str_seq()):
             for filename in files:
@@ -141,34 +156,60 @@ class CopyArtifactsPlugin(BeetsPlugin):
                 if len(file_ext) > 1 and file_ext[1:] in TYPES:
                     continue
 
-                if '.*' in self.extensions or file_ext in self.extensions:
-                    source_files.append(source_file)
-                else:
-                    ignored_files.append(source_file)
+                non_handled_files.append(source_file)
 
-        mapping = self._generate_mapping(item, album_path)
+        self._process_queue.extend([{
+            'files': non_handled_files,
+            'mapping': self._generate_mapping(item, dest_path)
+        }])
+        self._dirs_seen.extend([source_path])
 
-        self.process_artifacts(source_path, source_files, ignored_files, False, mapping)
+    def process_events(self):
+        print 'CLI exit'
+        import json
+        print self._dirs_seen
+        print json.dumps(self._process_queue, indent=4, sort_keys=True)
 
-    def process_artifacts(self, source_path, source_files, ignored_files, reimport=False, mapping=None):
+        for item in self._process_queue:
+            self.process_artifacts(item['files'], item['mapping'], False)
+
+
+    def process_artifacts(self, source_files, mapping, reimport=False):
+
+        ignored_files = []
+        source_path = os.path.dirname(source_files[0])
 
         if source_files:
             for source_file in source_files:
-
+                # os.path.basename() not suitable here as files may be contained
+                # within dir of source_path
                 filename = source_file[len(source_path)+1:]
-                if mapping:
-                    dest_file = self._destination(filename, mapping)
-                else:
-                    pass #XXX: set dest_file
 
+                dest_file = self._destination(filename, mapping)
+
+                # Skip as another plugin or beets has already moved this file
+                if not os.path.exists(source_file):
+                    ignored_files.append(source_file)
+                    continue
+
+                # Skip extensions not handled by plugin
+                file_ext = os.path.splitext(filename)[1]
+                if ('.*' not in self.extensions
+                    and file_ext not in self.extensions):
+                    ignored_files.append(source_file)
+                    continue
+
+                # Skip file if it already exists in dest
                 if (os.path.exists(dest_file)
                     and filecmp.cmp(source_file, dest_file)):
-                    # Skip file as it already exists in dest
                     ignored_files.append(source_file)
                     continue
 
                 dest_file = beets.util.unique_path(dest_file)
                 beets.util.mkdirall(dest_file)
+
+                # TODO: detect if beets was called with 'move' and override config
+                # option here
 
                 if config['import']['move']:
                     self._move_artifact(source_file, dest_file)
