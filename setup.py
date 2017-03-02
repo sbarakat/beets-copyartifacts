954e2bb9c11a00bb45c3c7ecb3007f9213b39a84
Sami Barakat <sami@sbarakat.co.uk>
2017-03-02 08:20:22 +0000

    added setup.py classifiers

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open('README.rst') as f:
     readme = f.read()
 
 setup(
-    name = "beets-copyartifacts",
-    version = "0.1.2",
+    name="beets-copyartifacts",
+    version="0.1.2",
     description="beets plugin to copy non-music files to import path",
     long_description=readme,
     author='Sami Barakat',
@@ -13,8 +13,19 @@ setup(
     url='https://github.com/sbarakat/beets-copyartifacts',
     download_url='https://github.com/sbarakat/beets-copyartifacts.git',
     license='MIT',
+    platforms='ALL',
 
     packages=['beetsplug'],
     namespace_packages=['beetsplug'],
-    install_requires = ['beets>=1.3.11']
+    install_requires=['beets>=1.3.11'],
+
+    classifiers=[
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Sound/Audio :: Players :: MP3',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ]
 )
